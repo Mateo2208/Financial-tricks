8d6b6730ae7aff35d80e826579a0e1014f17ec4e
Mateo2208 <123609083+Mateo2208@users.noreply.github.com>
2025-01-05 20:22:47 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+function updateSubMenu() {
+    const type = document.getElementById("type").value;
+    const incomeMenu = document.getElementById("incomeMenu");
+    const expenseMenu = document.getElementById("expenseMenu");
+
+    if (type === "Ingreso") {
+      incomeMenu.style.display = "block";
+      expenseMenu.style.display = "none";
+    } else if (type === "Egreso") {
+      expenseMenu.style.display = "block";
+      incomeMenu.style.display = "none";
+    } else {
+      incomeMenu.style.display = "none";
+      expenseMenu.style.display = "none";
+    }
+  }
+window.onload = function () {
+    const splashScreen = document.getElementById('splashScreen');
+    setTimeout(() => {
+      splashScreen.style.opacity = '0';
+      setTimeout(() => {
+        splashScreen.style.display = 'none';
+      }, 500);
+    }, 1200);
+  };
+  
+  // Manejar el envío del formulario
+  document.getElementById('financeForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+  
+    const popup = document.getElementById('popup');
+  
+    // Simula el envío al Google Sheet
+    fetch('https://tu-url-google-sheet.com', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        author: document.getElementById('author').value,
+        type: document.getElementById('type').value,
+        category:
+          document.getElementById('type').value === 'Ingreso'
+            ? document.getElementById('incomeCategory').value
+            : document.getElementById('expenseCategory').value,
+        amount: document.getElementById('amount').value,
+      }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          // Muestra el popup de éxito
+          popup.textContent = '¡Envío realizado con éxito!';
+          popup.className = ''; // Limpia clases previas (en caso de error)
+          popup.style.display = 'block';
+  
+          setTimeout(() => {
+            popup.style.display = 'none';
+          }, 1200); // Ocultar después de 3 segundos
+        } else {
+          throw new Error('Error en la respuesta del servidor');
+        }
+      })
+      .catch((error) => {
+        // Muestra el popup de error
+        popup.textContent = 'Error al enviar. Inténtalo nuevamente.';
+        popup.className = 'error';
+        popup.style.display = 'block';
+  
+        setTimeout(() => {
+          popup.style.display = 'none';
+        }, 3000); // Ocultar después de 3 segundos
+      });
+  });
+  
